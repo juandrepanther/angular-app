771df671c7335d67ce50d00d5d150f02cf1387fc
juandrepanther <juandre@inbox.lv>
2024-06-11 22:34:49 +0300

    feat: add input bind to form control

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule],
   templateUrl: './create-product.component.html',
 })
-export class CreateProductComponent {}
+export class CreateProductComponent {
+  form = new FormGroup({
+    name: new FormControl({ value: '', disabled: false }),
+  })
+
+  submit() {
+    console.log(this.form.value)
+  }
+}
