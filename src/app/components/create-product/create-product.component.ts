cbc1e3ab2193c318f7b756095d14efaa1bc4f9ac
juandrepanther <juandre@inbox.lv>
2024-06-12 14:25:58 +0300

    feat: add focus directive

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
+import { FocusDirective } from 'app/directives/focus.directive'
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [MatButtonModule, ReactiveFormsModule, CommonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, CommonModule, FocusDirective],
   templateUrl: './create-product.component.html',
 })
 export class CreateProductComponent {
---
+++ src/app/directives/focus.directive.ts
@@ -0,0 +1,18 @@
+import { AfterViewInit, Directive, ElementRef, OnInit } from '@angular/core'
+
+// just simply add this directive appFocus to the element you want to focus by default
+
+@Directive({
+  selector: '[appFocus]',
+  standalone: true,
+})
+export class FocusDirective implements OnInit, AfterViewInit {
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    // we can control the focus here
+    this.el.nativeElement.focus()
+  }
+
+  ngAfterViewInit(): void {}
+}
