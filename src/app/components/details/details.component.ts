f5530303bbd1a7379f113670cda102a9d0a89c59
juandrepanther <juandre@inbox.lv>
2024-06-14 13:32:18 +0300

    feat: animated details article

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -4,12 +4,25 @@ import { Product } from 'types'
 import { CommonModule } from '@angular/common'
 import { emptyProduct, truncateString } from 'lib/utils'
 import { ButtonComponent } from '../button/button.component'
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [MatCardModule, CommonModule, ButtonComponent],
   templateUrl: './details.component.html',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.3s', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.3s', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class DetailsComponent {
   product = input<Product>(emptyProduct)
